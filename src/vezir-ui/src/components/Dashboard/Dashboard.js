73f5191b4bc2db3a59312ec1f0562d427b1c397a
Alkan ARSLAN <alkanarslan@morhipo.com>
2022-01-23 15:00:07 +0300

    Add Layout

--- src/vezir-ui/src/components/Dashboard/Dashboard.js
+++ src/vezir-ui/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Grid
+      h="100vh"
+      templateRows="repeat(1, 1fr)"
+      templateColumns="repeat(2, 1fr)"
+      gap={4}
+    >
+      <GridItem rowSpan={1} colSpan={1} bg="tomato" />
+      <GridItem colSpan={1} bg="red" />
+    </Grid>
+  );
 }
